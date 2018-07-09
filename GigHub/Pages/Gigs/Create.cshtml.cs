74634592d4bd4abe3ac540b7df83693b5d26ae21
Ousmane Barry <VG5471@engie.com>
2018-07-09 17:14:38 +0200

    Create Add a Gig Form

---
+++ GigHub/Pages/Gigs/Create.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using GigHub.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GigHub.Pages.Gigs
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public GigFormViewModel GigViewModel { get; set; }
+
+        public CreateModel(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void OnGet()
+        {
+            GigViewModel = new GigFormViewModel { Genres = _dbContext.Genres.ToList() };
+        }
+    }
+}
