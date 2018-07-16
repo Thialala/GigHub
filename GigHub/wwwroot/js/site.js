a7e826893f9bedda7e3e80533ace47392d30c674
Ousmane Barry <obarrypublic@gmail.com>
2018-07-16 16:58:52 +0200

    Move the javascript code to site.js to allow it to execute because it doesn't in a partial view.

--- GigHub/wwwroot/js/site.js
+++ GigHub/wwwroot/js/site.js
@@ -2,3 +2,40 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$(document).ready(function () {
+    $(".js-toggle-attendance").click(function (e) {
+        var button = $(e.target);
+        $.ajax({
+            url: "/api/attendances",
+            contentType: "application/json; charset=utf-8",
+            type: "POST",
+            data: JSON.stringify({ gigId: button.attr("data-gig-id") })
+        })
+            .done(function () {
+                button
+                    .removeClass("btn-default")
+                    .addClass("btn-info")
+                    .text("Going");
+            })
+            .fail(function () {
+                alert("Something failed!");
+            });
+    });
+
+    $(".js-toggle-following").click(function (e) {
+        var button = $(e.target);
+        $.ajax({
+            url: "/api/followings",
+            contentType: "application/json; charset=utf-8",
+            type: "POST",
+            data: JSON.stringify({ followeeId: button.attr("data-following-id") })
+        })
+            .done(function () {
+                button
+                    .text("Following");
+            })
+            .fail(function () {
+                alert("Something failed!");
+            });
+    });
+});
