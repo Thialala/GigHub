06d51f7c61beac0f316ae920ca4d5e7f18fa7ba9
Ousmane Barry <obarrypublic@gmail.com>
2018-07-19 16:27:07 +0200

    Redesign to be sure that UserNotification is always in valid state

--- GigHub/Models/ApplicationUser.cs
+++ GigHub/Models/ApplicationUser.cs
@@ -7,22 +7,16 @@ namespace GigHub.Models
     {
         public string Name { get; set; }
 
-        public ICollection<ApplicationUser> Followees { get; set; } = new List<ApplicationUser>();
+        public ICollection<ApplicationUser> Followees { get; private set; } = new List<ApplicationUser>();
 
-        public ICollection<ApplicationUser> Followers { get; set; } = new List<ApplicationUser>();
+        public ICollection<ApplicationUser> Followers { get; private set; } = new List<ApplicationUser>();
 
-        public ICollection<UserNotification> UserNotifications { get; set; } = new List<UserNotification>();
+        public ICollection<UserNotification> UserNotifications { get; private set; } = new List<UserNotification>();
 
         public void Notify(Notification notification)
         {
-            var userNotification = new UserNotification
-            {
-                User = this,
-                Notification = notification
-            };
-
+            var userNotification = new UserNotification(this, notification);
             UserNotifications.Add(userNotification);
         }
-
     }
 }
--- GigHub/Models/UserNotification.cs
+++ GigHub/Models/UserNotification.cs
@@ -1,13 +1,21 @@
-﻿namespace GigHub.Models
+﻿using System;
+
+namespace GigHub.Models
 {
     public class UserNotification
     {
-        public string UserId { get; set; }
-        public ApplicationUser User { get; set; }
+        public string UserId { get; private set; }
+        public ApplicationUser User { get; private set; }
 
-        public int NotificationId { get; set; }
-        public Notification Notification { get; set; }
+        public int NotificationId { get; private set; }
+        public Notification Notification { get; private set; }
 
         public bool IsRead { get; set; }
+
+        public UserNotification(ApplicationUser user, Notification notification)
+        {
+            User = user ?? throw new ArgumentNullException(nameof(user));
+            Notification = notification ?? throw new ArgumentNullException(nameof(notification));
+        }
     }
 }
